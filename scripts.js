7494ad774c83389f8f0068356786e7cad1bbd4cb
SGVcode <sgvero.box@gmail.com>
2023-02-06 12:08:58 -0300

    Corregidos comentaios

--- scripts.js
+++ scripts.js
@@ -31,10 +31,10 @@ function handleSubmit(event){
   $botonMailto.click()
 }
 
-// Get the button:
+// Traer Botón Ir Arriba:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// Mostrar Botón al hacer 20 px de scroll abajo
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -45,7 +45,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// Ir Arriba al hacer click en el botón
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
